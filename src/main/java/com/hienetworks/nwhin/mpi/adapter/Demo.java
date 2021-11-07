7596bd821400fba3fd41294bc44bb89efbc119f6
alan uhl <alan.uhl@centralishealth.com>
2021-11-07 11:37:48 -0500

    Misc changes

--- src/main/java/com/hienetworks/nwhin/mpi/adapter/Demo.java
+++ src/main/java/com/hienetworks/nwhin/mpi/adapter/Demo.java
@@ -1,34 +1,44 @@
 package com.hienetworks.nwhin.mpi.adapter;
 
-//import org.datacontract.schemas._2004._07.hielibrary.PatientEntity;
+// import com.hienetworks.nwhin.mpi.adapter;
 
-import gov.hhs.fha.nhinc.mpilib.Patient;
+import org.hl7.v3.RespondingGatewayPRPAIN201305UV02RequestType;
 
-
+// TODO:Refernces to auto gen'd code, so will be transient and likely need to be nixed
+// import org.datacontract.schemas._2004._07.hielibrary.PatientEntity;
+// import gov.hhs.fha.nhinc.mpilib.Patient;
+ 
 public class Demo {
 
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+        
+        // TODO Auto-generated method stub
         try
         {
             HIENPatientDiscoverySoapClient client =  HIENPatientDiscoverySoapClient.getInstance();
-            PatientInfo patientInfo = new PatientInfo();
+            
+
+           PatientInfo patientInfo = new PatientInfo();
+
+//         Note: Real/existing Patient from real repo                      
 //            patientInfo.setLname("jones");
 //            patientInfo.setFname("curtis");
 //            patientInfo.setGender("M");
 //            patientInfo.setSSN("265662107");
 //            patientInfo.setDob("1944-06-23");
 
+//         Note: Test patient from a test repo
             patientInfo.setLname("davidson");
             patientInfo.setFname("amy");
             patientInfo.setGender("f");
             patientInfo.setDob("1983-10-17");
 
-            PatientRMPIEntity patientList = client.sendData(patientInfo);
-//            HIENMPIAdapter responseClient = new HIENMPIAdapter();
-////          Patient p = new Patient();
-//            responseClient.ProcessPatientList(patientList);
-
+            PatientRMPIEntity patientList = null; //client.sendData(patientInfo);
+            HIENMPIAdapter adapter = new HIENMPIAdapter();
+            RespondingGatewayPRPAIN201305UV02RequestType request = null;
+            adapter.FetchPatient(patientList, patientInfo);
+            
+            // Testing that the singleton / static instance continues to work after the first run
             HIENPatientDiscoverySoapClient client2 =  HIENPatientDiscoverySoapClient.getInstance();
             PatientRMPIEntity patientList2 = client2.sendData(patientInfo);
 
