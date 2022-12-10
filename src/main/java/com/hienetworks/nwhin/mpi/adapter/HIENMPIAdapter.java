29ba0749ed7b7a75127461c02386e31de819515c
alan uhl <alan.uhl@centralishealth.com>
2022-12-10 12:03:55 -0500

    Added reasonOf element to filtered PD response

--- src/main/java/com/hienetworks/nwhin/mpi/adapter/HIENMPIAdapter.java
+++ src/main/java/com/hienetworks/nwhin/mpi/adapter/HIENMPIAdapter.java
@@ -34,9 +34,17 @@ import gov.hhs.fha.nhinc.properties.PropertyAccessor;
 import java.time.Instant;
 import java.util.List;
 import java.util.logging.Level;
+import org.hl7.v3.ANY;
 import org.hl7.v3.CS;
+import org.hl7.v3.EDExplicit;
 import org.hl7.v3.II;
+//import org.hl7.v3.t
 import org.hl7.v3.MFMIMT700711UV01QueryAck;
+import org.hl7.v3.MFMIMT700711UV01Reason;
+import org.hl7.v3.MCAIMT900001UV01DetectedIssueEvent;
+import org.hl7.v3.MCAIMT900001UV01DetectedIssueManagement;
+import org.hl7.v3.MCAIMT900001UV01SourceOf;
+import org.hl7.v3.XActMoodDefEvn;
 
 /**
  * Patient Discovery (PD) / Master Patient Index (eMPI) CONNECT Adapter
@@ -53,10 +61,25 @@ public class HIENMPIAdapter implements gov.hhs.fha.nhinc.adaptercomponentmpi.Ada
     
     private static final Logger logger = Logger.getLogger(HIENMPIAdapter.class);
     final private PropertyAccessor propertyAccessor = PropertyAccessor.getInstance();
+    final private String REASONOF_ACT_ADMIN_DETECTED_ISSUE_CODE = "ActAdministrativeDetectedIssueCode";
+    final private String REASONOF_FILTER_REASON = "Patient not a FL resident, filtering.";
+    final private String REASONOF_MITIGATEDBY_TYPECODE = "MITGT";
+    final private String REASONOF_DETECTEDISSUEMANAGEMENT_CODE = "AnswerNotAvailable";
+    final private String REASONOF_DETECTEDISSUEMANAGEMENT_CODESYSTEM = "1.3.6.1.4.1.19376.1.2.27.3";
+    final private String REASONOF_DETECTEDISSUEMANAGEMENT_CLASSCODE_ALERT = "ALRT";
     private String FilterSenders = "";
     private String FilterAllowStates = "";
     private String SenderHCID = null;
 
+    /**
+     * PD-specific Exception
+     */
+    public class PatientDiscoverySoapClientException extends Exception {
+        public PatientDiscoverySoapClientException(String message) {
+            super(message);
+        }
+    }    
+    
     /**
      * Find documents (XDSDocumentEntry objects) in the registry for a given patientID with a matching 
      * availabilityStatus attribute. The other parameters can be used to restrict the set of XDSDocumentEntry objects 
@@ -66,13 +89,15 @@ public class HIENMPIAdapter implements gov.hhs.fha.nhinc.adaptercomponentmpi.Ada
      */
     @Override
     public PRPAIN201306UV02 findCandidates(RespondingGatewayPRPAIN201305UV02RequestType request) {
-        PRPAIN201306UV02 oResponse = null;
+        
+        PRPAIN201306UV02 oResponse = new PRPAIN201306UV02();        
         String fName = null;
         String lName = null;
         String gender;
         String dob;
         PatientInfo patientInfo = new PatientInfo();
-            
+        
+        
         try {
             logger.info("In HIENMPIAdapter");
 
@@ -81,7 +106,7 @@ public class HIENMPIAdapter implements gov.hhs.fha.nhinc.adaptercomponentmpi.Ada
             // 2.) Create JSON Object using the request parameters to send RESTful call to get Patient details
             // 3.) Read response from JSON response Object and build/package up PRPAIN201306UV02 response 
             // ******************************************************************************************************            
-            oResponse = new PRPAIN201306UV02();
+
             PRPAIN201305UV02 oPRPAIN201305UV02 = request.getPRPAIN201305UV02();
             PRPAIN201305UV02QUQIMT021001UV01ControlActProcess controlActProcess = oPRPAIN201305UV02.getControlActProcess();
             if (controlActProcess != null) {
@@ -138,12 +163,12 @@ public class HIENMPIAdapter implements gov.hhs.fha.nhinc.adaptercomponentmpi.Ada
 
                     long after = Instant.now().toEpochMilli();                    
                     logger.info("HIENMPIAdapter Transaction Duration: " + (after-before));
+
+                    final Patients patList = new Patients();
                     
                     if (null != patientResponse) {
 
                         logger.info("Response msg: " + patientResponse);
-
-                        final Patients patList = new Patients();
 
                         String errorMsg = patientResponse.getError().getValue();
 
@@ -204,8 +229,7 @@ public class HIENMPIAdapter implements gov.hhs.fha.nhinc.adaptercomponentmpi.Ada
 
                     } else {
                         logger.info("Null Patient Response - responding with empty list");
-                        Patients patListempty = new Patients();
-                        oResponse = HL7Parser201306.buildMessageFromMpiPatient(patListempty, oPRPAIN201305UV02);
+                        oResponse = HL7Parser201306.buildMessageFromMpiPatient(patList, oPRPAIN201305UV02);
                     }
                 } else {
                     logger.info("FILTERED PATIENT");
@@ -222,44 +246,131 @@ public class HIENMPIAdapter implements gov.hhs.fha.nhinc.adaptercomponentmpi.Ada
     /*
     ************************************************************************************
     Example/Sample PD ACK snippet showing the typeCode and queryResponseCode 'AE' values 
-    that are required for the VA filtration PD/ITI-55 responsesk
+    and reasonOf element/element assembly that are required for the VA filtration 
+    PD/ITI-55 responses
     ************************************************************************************
-        <acknowledgement>
-            <typeId root="2.16.840.1.113883.1.6" extension="PRPA_IN201305UV02"/>
-            <typeCode code="AE"/>
-            <targetMessage>
-                <id root="1.3.6.1.4.1.12559.11.24.4.1.17.1" extension="12432"/>
-            </targetMessage>
-        </acknowledgement>
-        <controlActProcess classCode="CACT" moodCode="EVN">
-            <code code="PRPA_TE201306UV02" codeSystem="2.16.840.1.113883.1.6"/>
-            <queryAck>
-                <queryId root="1.3.6.1.4.1.12559.11.24.4.1.17.2" extension="12431"/>
-                <statusCode code="deliveredResponse"/>
-                <queryResponseCode code="AE"/>
-            </queryAck>
+    <acknowledgement>
+        <typeId root="2.16.840.1.113883.1.6" extension="PRPA_IN201305UV02"/>
+        <typeCode code="AE"/>
+        <targetMessage>
+            <id root="1.3.6.1.4.1.12559.11.24.4.1.17.1" extension="12432"/>
+        </targetMessage>
+    </acknowledgement>
+    <controlActProcess classCode="CACT" moodCode="EVN">
+        <code code="PRPA_TE201306UV02" codeSystem="2.16.840.1.113883.1.6"/>
+
+        *********************************************************************************
+
+        <reasonOf typeCode="RSON">
+            <detectedIssueEvent classCode="ALRT"
+                    moodCode="EVN">
+                <code code="ActAdministrativeDetectedIssueCode"
+                      codeSystem="2.16.840.1.113883.5.4"/>
+                <text>Patient not a FL resident, filtering.</text>
+                <mitigatedBy typeCode="MITGT">
+                    <detectedIssueManagement classCode="ALRT ACT"
+                        moodCode="EVN">
+                        <code code="AnswerNotAvailable"
+                            codeSystem="1.3.6.1.4.1.19376.1.2.27.3"/>
+                    </detectedIssueManagement>
+                </mitigatedBy>
+            </detectedIssueEvent>
+        </reasonOf>    
+
+        ********************************************************************************
+
+        <queryAck>
+            <queryId root="1.3.6.1.4.1.12559.11.24.4.1.17.2" extension="12431"/>
+            <statusCode code="deliveredResponse"/>
+            <queryResponseCode code="AE"/>
+        </queryAck>
+    
     ****************************************************************************************
     */
     
-    private  PRPAIN201306UV02 buildMessageForFilteredPatientResponse(PRPAIN201305UV02 oPRPAIN201305UV02) {
+    private PRPAIN201306UV02 buildMessageForFilteredPatientResponse(PRPAIN201305UV02 oPRPAIN201305UV02) {
         PRPAIN201306UV02 response;
         MFMIMT700711UV01QueryAck queryAck;
         Patients patListempty = new Patients();
 
-        // Build Ack-status coded-string element
-        CS ackStatus = new CS();
-        ackStatus.setCode("AE");
-
         // Build boiler-plate response
         response = HL7Parser201306.buildMessageFromMpiPatient(patListempty, oPRPAIN201305UV02);        
 
-        // Update acknowledgement typeCode (see example above)
-        response.getAcknowledgement().get(0).setTypeCode(ackStatus);
+        try {
+            
+            // Build Ack-status coded-string element
+            CS ackStatus = new CS();
+            ackStatus.setCode("AE");
+
+            // Update acknowledgement typeCode (see example above)
+            response.getAcknowledgement().get(0).setTypeCode(ackStatus);
+
+            // Update controlActProcess queryAck (see example above)
+            response.getControlActProcess().getQueryAck().setQueryResponseCode(ackStatus);
+
+            // Build the reasonOf element to document the reason for rejecting/filtering the PD request
+            MFMIMT700711UV01Reason reasonOf = reasonOfBuildElement(response);
+
+            // Add reasonOf element to response
+            response.getControlActProcess().getReasonOf().add(reasonOf);
+            
+            logger.info("Added reasonOf Element to PD Response: "+response.getControlActProcess());            
         
-        // Update controlActProcess queryAck (see example above)
-        response.getControlActProcess().getQueryAck().setQueryResponseCode(ackStatus);
+        } catch (Exception exp) {
+            logger.error("Error Composing Filtered PD Response: " + exp);
+        }
         
         return response;
+    }
+
+    /*
+     * VA special - Build reasonOf individual elements to incicate reason for PD filtration/rejection
+     */
+
+    private MFMIMT700711UV01Reason reasonOfBuildElement(PRPAIN201306UV02 response) {
+        
+        // Create the root reason/why PD not-found (filtered) response root element
+        MFMIMT700711UV01Reason reasonOf = new MFMIMT700711UV01Reason();
+        
+        // Create the next associated event that's causing the not-found compoud child element
+        MCAIMT900001UV01DetectedIssueEvent issueEvent = new MCAIMT900001UV01DetectedIssueEvent();
+        issueEvent.getClassCode().add(REASONOF_DETECTEDISSUEMANAGEMENT_CLASSCODE_ALERT);
+        issueEvent.getMoodCode().add(XActMoodDefEvn.EVN.name());
+        CS issueCode = new CS();
+        issueCode.setCode(REASONOF_ACT_ADMIN_DETECTED_ISSUE_CODE);
+        issueEvent.setCode(issueCode);
+        EDExplicit text = new EDExplicit();
+        text.setLanguage(REASONOF_FILTER_REASON);
+        issueEvent.setText(text);
+        
+        // Create the mitigation actor compound child element
+        MCAIMT900001UV01SourceOf mitigatedBy = new MCAIMT900001UV01SourceOf();
+        II typeId = new II();
+        typeId.setRoot(REASONOF_MITIGATEDBY_TYPECODE);
+        mitigatedBy.setTypeId(typeId);
+        
+        // Setup 'issue' management mitigatedBy content/element
+        MCAIMT900001UV01DetectedIssueManagement issueMgmt = new MCAIMT900001UV01DetectedIssueManagement();        
+        CS mgmtCode = new CS();
+        mgmtCode.setCode(REASONOF_DETECTEDISSUEMANAGEMENT_CODE);
+        mgmtCode.setCodeSystem(REASONOF_DETECTEDISSUEMANAGEMENT_CODESYSTEM);
+        issueMgmt.setCode(mgmtCode);
+        issueMgmt.setMoodCode(XActMoodDefEvn.EVN);      
+        
+        /*
+        * Assemble the reasonOf from above pieces/parts
+        */
+        
+        // Issue Mgmt -> Mitigated By
+        mitigatedBy.setDetectedIssueManagement(issueMgmt);
+        
+        // Mitigated By -> Issue Event
+        issueEvent.getMitigatedBy().add(mitigatedBy);
+        
+        // Issue Event -> Reason Of
+        reasonOf.setDetectedIssueEvent(issueEvent);
+
+        return reasonOf;     
     }
 
     /**
@@ -348,15 +459,6 @@ public class HIENMPIAdapter implements gov.hhs.fha.nhinc.adaptercomponentmpi.Ada
         } catch (PropertyAccessException ex) {
             java.util.logging.Logger.getLogger(HIENMPIAdapter.class.getName()).log(Level.SEVERE, null, ex);
             logger.error("Error Loading Filter Rules: " + ex);
-        }
-    }
-    
-    /**
-     *
-     */
-    public class PatientDiscoverySoapClientException extends Exception {
-        public PatientDiscoverySoapClientException(String message) {
-            super(message);
         }
     }
 }
